d21ae68990288101337acc61babbac1d481a1adc
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-16 00:27:57 +0530

    Feat: Added Admin>GetScores Routes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
+const { authenticateAdmin } = require("../middleware/auth");
 
 router.post("/register", adminController.register);
 router.post("/login", adminController.login);
+router.get("/scores", authenticateAdmin, adminController.getScores);
 
 module.exports = router;
