e8fe70d43a40cf2e8ec7ab778961695608550d8e
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-16 00:15:32 +0530

    Feat: Added Admin>Login Routes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 
 router.post("/register", adminController.register);
+router.post("/login", adminController.login);
 
 module.exports = router;
