0b97d944cae7621f5867e2ee59b63a359e42ab10
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-15 23:41:31 +0530

    Feat: Socket Configration

---
+++ server/config/socket.js
@@ -0,0 +1,32 @@
+const socketIo = require("socket.io");
+const logger = require("../utils/logger");
+
+let io;
+
+const initializeSocket = (server) => {
+  io = socketIo(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    logger.info(`Client connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+      logger.info(`Client disconnected: ${socket.id}`);
+    });
+  });
+
+  return io;
+};
+
+const getIO = () => {
+  if (!io) {
+    throw new Error("Socket.io not initialized!");
+  }
+  return io;
+};
+
+module.exports = { initializeSocket, getIO };
