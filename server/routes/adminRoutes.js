e555e1c8991df47c5936449dd3a1540d400d370d
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-16 00:31:07 +0530

    Feat: Added Admin>CreatQuestion Routes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -6,5 +6,6 @@ const { authenticateAdmin } = require("../middleware/auth");
 router.post("/register", adminController.register);
 router.post("/login", adminController.login);
 router.get("/scores", authenticateAdmin, adminController.getScores);
+router.post("/questions", authenticateAdmin, adminController.createQuestion);
 
 module.exports = router;
