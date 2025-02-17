722bea0ac75320a6d65d22874d8b2b8f07e2b096
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-17 21:52:25 +0530

    feat(model): update Question model schema and methods

--- server/models/Question.js
+++ server/models/Question.js
@@ -1,13 +1,50 @@
 const mongoose = require("mongoose");
 
-const questionSchema = new mongoose.Schema({
-  round: { type: Number, required: true },
-  questionNumber: { type: Number, required: true },
-  content: { type: String, required: true },
-  answer: { type: String, required: true },
-  type: { type: String },
-  points: { type: Number, required: true },
-  hints: [{ type: String }],
+const questionSchema = new mongoose.Schema(
+  {
+    round: {
+      type: Number,
+      required: true,
+      enum: [1, 2, 3],
+    },
+    questionNumber: {
+      type: Number,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    answer: {
+      type: String,
+      required: true,
+    },
+    points: {
+      type: mongoose.Schema.Types.Mixed,
+      required: true,
+    },
+    hints: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+questionSchema.index({ round: 1, questionNumber: 1 }, { unique: true });
+
+questionSchema.post("save", function (error, doc, next) {
+  if (error.name === "MongoServerError" && error.code === 11000) {
+    next(
+      new Error(
+        `Question number ${doc.questionNumber} already exists in round ${doc.round}`
+      )
+    );
+  } else {
+    next(error);
+  }
 });
 
-module.exports = mongoose.model("Question", questionSchema);
+const Question = mongoose.model("Question", questionSchema);
+module.exports = Question;
