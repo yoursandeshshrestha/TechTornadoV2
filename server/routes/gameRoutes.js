18bbce40d039500f49a2b29b54cc41be20b044a0
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-17 21:53:16 +0530

    feat(routes): add new routes for game operations

---
+++ server/routes/gameRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const {
+  handleSubmitAnswer,
+  handleSkipQuestion,
+  handleGetLeaderboard,
+  handleGetQuestion,
+  handleGetAllQuestionsByRound,
+} = require("../controllers/gameController");
+const { authenticateTeam } = require("../middleware/auth");
+
+router.post("/submit-answer", authenticateTeam, handleSubmitAnswer);
+router.get(
+  "/questions/:roundNumber/:questionNumber",
+  authenticateTeam,
+  handleGetQuestion
+);
+router.get(
+  "/questions/round/:roundNumber",
+  authenticateTeam,
+  handleGetAllQuestionsByRound
+);
+router.post("/skip-question", authenticateTeam, handleSkipQuestion);
+router.get("/leaderboard", handleGetLeaderboard);
+
+module.exports = router;
