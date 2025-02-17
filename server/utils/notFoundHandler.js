70796215848160ed2df2bd8a7c22400e00795e84
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-17 21:36:40 +0530

    Removed Comments

--- server/utils/notFoundHandler.js
+++ server/utils/notFoundHandler.js
@@ -4,10 +4,8 @@ module.exports = (req, res, next) => {
   const error = new Error(`Route ${req.originalUrl} not found`);
   error.status = 404;
 
-  // Log the error
   logger.warn(`${req.method} ${req.originalUrl} - Route not found`);
 
-  // Send detailed error in development, generic message in production
   const response = {
     message:
       process.env.NODE_ENV === "development"
