afb6cc666cc6303a756f9948b1501b20e5b2fca6
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-17 21:37:07 +0530

    Updated function to calculatePoints

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,20 +1,19 @@
-const calculatePoints = (round, attempts, isCorrect) => {
-  if (!isCorrect) return 0;
-
-  if (round === 3) {
-    switch (attempts) {
-      case 1:
-        return 30;
-      case 2:
-        return 20;
-      case 3:
-        return 10;
-      default:
-        return 0;
-    }
+const calculatePoints = (round) => {
+  switch (round) {
+    case 1:
+      return 1;
+    case 2:
+      return 5;
+    case 3:
+      return {
+        firstAttempt: 30,
+        secondAttempt: 20,
+        thirdAttempt: 10,
+        failed: 0,
+      };
+    default:
+      throw new Error("Invalid round number");
   }
-
-  return round === 1 ? 1 : 5;
 };
 
 const getRoundDuration = (round) => {
