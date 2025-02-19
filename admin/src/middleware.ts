ff30b1fe6f377f34cb09a9410467d1fba6d30b17
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-19 21:00:26 +0530

    Feat: Middleware for protected route

---
+++ admin/src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token");
+  const isAuthPage = request.nextUrl.pathname === "/login";
+
+  if (!token && !isAuthPage) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (token && isAuthPage) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard", "/login"],
+};
