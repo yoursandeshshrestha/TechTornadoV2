d76e0a1e88cde3e119e6cb148142be6e12fbfa97
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-20 23:04:19 +0530

    Feat: Add new parameter for game status

--- server/models/gameState.js
+++ server/models/gameState.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const gameStateSchema = new mongoose.Schema({
   currentRound: { type: Number, default: 1 },
   isRegistrationOpen: { type: Boolean, default: true },
+  isGameActive: { type: Boolean, default: false },
   roundStartTime: { type: Date },
   roundEndTime: { type: Date },
 });
