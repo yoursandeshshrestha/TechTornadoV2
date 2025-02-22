44ae7bc0c48a162b620b9b41a6b9c1380332a5ea
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-22 19:03:55 +0530

    refactor: added remainingTime and ispaused in schema

--- server/models/gameState.js
+++ server/models/gameState.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const gameStateSchema = new mongoose.Schema({
-  currentRound: { type: Number, default: 1 },
-  isRegistrationOpen: { type: Boolean, default: true },
+  currentRound: { type: Number, default: 0 },
+  isRegistrationOpen: { type: Boolean, default: false },
   isGameActive: { type: Boolean, default: false },
   roundStartTime: { type: Date },
   roundEndTime: { type: Date },
+  remainingTime: { type: Number }, // Store remaining time when paused
+  isPaused: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("GameState", gameStateSchema);
