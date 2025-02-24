2291bf8aaf894e8a87b0117db4742ed80f741cdf
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2025-02-24 13:21:03 +0530

    Feat: Redux Store

--- admin/src/lib/redux/store.ts
+++ admin/src/lib/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
+import gameReducer from "./features/gameSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    game: gameReducer, // Added game reducer
   },
 });
 
